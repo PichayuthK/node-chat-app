9ff59b8a70ad295d38a1869c12ec59a6a1804515
Peace PC <pichayuthkitti@gmail.com>
2017-10-22 18:04:45 +0700

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+    //selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+
+};
+
 socket.on('connect', function () {
     console.log('connected to server');
 
@@ -17,14 +34,14 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
     var template = jQuery('#message-template').html();
-    var html = Mustache.render(template,{
+    var html = Mustache.render(template, {
         text: message.text,
         from: message.from,
         createdAt: formattedTime
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
 
     // var li = jQuery('<li></li>');
     // li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
@@ -38,11 +55,12 @@ socket.on('newLocationMessage', function (message) {
     var template = jQuery('#location-message-template').html();
     var html = Mustache.render(template, {
         url: message.url,
-        from:message.from,
+        from: message.from,
         createdAt: message.createdAt
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
     // var li = jQuery('<li></li>');
     // var a = jQuery('<a target="_blank">My current location</a>');
 
